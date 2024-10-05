090b928e623491299710accd0a80f46210989a68
Yakovlev-Melarn <melarn4u@gmail.com>
2024-10-05 13:47:21 +0300

    TG

--- app/Http/Libs/Telegramm.php
+++ app/Http/Libs/Telegramm.php
@@ -11,7 +11,7 @@ class Telegramm
         Http::withHeaders([])
             ->timeout(3600)
             ->acceptJson()
-            ->post("https://api.telegram.org/bot1667207989:AAEph_dWGYM1oX8HEKDy0PPNVVAkU4ry0VE/sendMessage", [
+            ->post("https://api.telegram.org/|||/sendMessage", [
                 'chat_id' => "1139134460",
                 'text' => $text
             ]);
