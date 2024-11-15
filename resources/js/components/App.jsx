3d6e126f621e9b69c6f0583a3bf1b5d021152085
Yakovlev-Melarn <melarn4u@gmail.com>
2024-11-15 11:33:43 +0300

    SellerList

--- resources/js/components/App.jsx
+++ resources/js/components/App.jsx
@@ -1,11 +1,52 @@
 import NavHeader from "./NavHeader";
 import Header from "./Header";
+import {useEffect, useState} from "react";
 
 export default function App() {
-    return (
-        <div>
-            <NavHeader/>
-            <Header/>
-        </div>
-    )
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [sellers, setSellers] = useState([]);
+    function getToken(){
+        return document.querySelector(`meta[name="token"]`)
+            .getAttribute('content');
+    }
+    useEffect(() => {
+        fetch("http://127.0.0.1:8000/api/getSellerList", {
+            method: 'get',
+            headers: new Headers({
+                'authorization': getToken()
+            })
+        })
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setSellers(result);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }, [])
+    if (error) {
+        return <div>Ошибка: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Загрузка...</div>;
+    } else {
+        return (
+            <div>
+                <NavHeader/>
+                <Header/>
+                <ul>
+                    {sellers.map(item => (
+                        <li key={item.id}>
+                            {item.name} {item.price}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+        );
+    }
 }
