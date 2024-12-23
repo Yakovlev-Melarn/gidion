4d956e4932252b477478683362c1d98168f3dca4
Yakovlev-Melarn <melarn4u@gmail.com>
2024-12-23 10:49:08 +0300

    231224

--- resources/js/components/Index/Index.jsx
+++ resources/js/components/Index/Index.jsx
@@ -16,11 +16,31 @@ export default function Index(props) {
                                 <GetDayLink chart={props.chart} seller={props.seller} day={props.dates.prevDay}
                                             name="предыдущий день"/>
                                 <GetDayLink chart={props.chart} seller={props.seller} day={props.dates.nextDay}
-                                              name="следующий день"/>
+                                            name="следующий день"/>
                             </div>
                         </div>
                         <div className="card-body">
                             <RenderLineChart data={props.data}/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-xl-3 col-xxl-4 col-lg-6 col-sm-6">
+                    <div className="widget-stat card">
+                        <div className="card-body p-4">
+                            <div className="media">
+									<span className="mr-3">
+										<i className="flaticon-091-shopping-cart"/>
+									</span>
+                                <div className="media-body text-white text-right">
+                                    <p className="mb-1">Заказано</p>
+                                    <h3 className="text-white"><a href="#" className="tOrders">1000 ₽</a></h3>
+                                    <p className="mb-0 fs-13">
+                                        <span className="text-warning mr-1">3</span> шт.
+                                    </p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
